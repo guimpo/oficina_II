8c5992ec91082012092232bfb2896ee6eff0ddc9
guimpo <nakaima.prk@gmail.com>
2017-11-03 01:27:42 -0200

    Adicionado consign

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import express from "express"
+import express from "express";
+import consign from "consign";
+
 const PORT = 3000
 const app = express()
 
+app.set('json spaces', 4)
+
+consign()
+  .include('routes')
+  .into(app)
 
 app.listen(PORT, () => console.log(`backend - Port ${PORT}`))
