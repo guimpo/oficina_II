78352893eb51e45c8b6bc5a04c6662da1b195da5
guimpo <nakaima.prk@gmail.com>
2017-11-03 01:24:50 -0200

    Movido código para routes

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import express from "express"
 const PORT = 3000
 const app = express()
 
-app.get("/", (req, res) => res.json({ status: "Rodando" }))
+
 app.listen(PORT, () => console.log(`backend - Port ${PORT}`))
