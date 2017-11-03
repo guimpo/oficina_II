ea3c376ef6b2fe1272d4e863d17c925e6b0f1567
guimpo <nakaima.prk@gmail.com>
2017-11-03 00:56:10 -0200

    Criado servidor

---
+++ servidor.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+var Express = require('express')
+var app = Express()
+
+app.use(Express.static(__dirname + '/static'))
+
+app.get('/', function (req, res, next) {
+  try {
+    fs.readFile(__dirname + '/views/login.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    })
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.listen(process.env.PORT || 3001, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
