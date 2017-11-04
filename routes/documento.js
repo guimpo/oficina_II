a5ce062d726da42d1e92dceea16f3eb3aad6f3a3
guimpo <nakaima.prk@gmail.com>
2017-11-04 08:22:48 -0200

    Adicionada rota para upload de arquivos

--- routes/documento.js
+++ routes/documento.js
@@ -1,3 +1,16 @@
+var multer = require('multer')
+
+var Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./imagens");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  }
+});
+
+var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count
+
 module.exports = app => {
   const Documento = app.db.models.Documento
   const Usuario = app.db.models.Usuario
@@ -32,11 +45,29 @@ module.exports = app => {
         })
     })
   
+  app.post("/Documentos/upload", (req, res) => {
+    upload(req, res, function (err) {
+      if (err) {
+        return res.end("Something went wrong!");
+      }
+      return res.end("File uploaded sucessfully!.");
+    });
+  })
+
   app.post("/Documentos", (req, res) => {
-    Documento.create(req.body)
-      .then(result => res.json(result))
-      .catch(error => {
-        res.status(412).json({ msg: error.message })
-      })
+    upload(req, res, function (err) {
+      if (err) {
+        return res.end("Something went wrong!");
+      }
+      return res.end("File uploaded sucessfully!.");
+    });
+
+
+
+    // Documento.create(req.body)
+    //   .then(result => res.json(result))
+    //   .catch(error => {
+    //     res.status(412).json({ msg: error.message })
+    //   })
   })
 }
