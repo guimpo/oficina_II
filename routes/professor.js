48490d6e9b426ad35915e5f67e205edc28ce4910
guimpo <nakaima.prk@gmail.com>
2017-11-04 02:25:17 -0200

    Adicionada rota para professor

---
+++ routes/professor.js
@@ -0,0 +1,30 @@
+module.exports = app => {
+  const Professor = app.db.models.Professor
+  const Usuario = app.db.models.Usuario
+  
+  app.route("/Professor")
+    .all(app.auth.authenticate())
+    .get((req, res) => {
+      Professor.findById(req.professor.id, {
+        attributes: ["id", "nome", "email"]
+      })
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({ msg: error.message })
+        })
+    })
+    .delete((req, res) => {
+      Professor.destroy({ where: { id: req.professor.id } })
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({ msg: error.message })
+        })
+    })
+  app.post("/Professores", (req, res) => {
+    Professor.create(req.body)
+      .then(result => res.json(result))
+      .catch(error => {
+        res.status(412).json({ msg: error.message })
+      })
+  })
+}
