369a15cd047484ba32e236da48ea25c57001d569
guimpo <nakaima.prk@gmail.com>
2017-11-04 02:37:16 -0200

    Criado modelo documento

---
+++ models/documento.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  var Documento = sequelize.define("Documento", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+      tableName: "Documento"
+    }
+  )
+
+  Documento.associate = function (models) {
+    Documento.belongsTo(models.Aluno, {foreignKey: "alunoId"})
+  }
+
+  return Documento
+}
