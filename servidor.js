41116d0aaf998e65f56b37fb778397813d4277c0
guimpo <nakaima.prk@gmail.com>
2017-11-04 04:04:44 -0200

    Adicionada tela de cadastro de professor

--- servidor.js
+++ servidor.js
@@ -16,6 +16,18 @@ app.get('/', function (req, res, next) {
   }
 })
 
+app.get('/CadastrarProfessor', function (req, res, next) {
+  try {
+    fs.readFile(__dirname + '/views/cadastrarProfessor.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    })
+  } catch (e) {
+    next(e)
+  }
+})
+
 app.listen(process.env.PORT || 3001, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3001))
 })
