2f1013a7b8294bd2f9d912950b7f3b43ff74b637
guimpo <nakaima.prk@gmail.com>
2017-11-04 02:46:36 -0200

    Criado modelo item

---
+++ models/item.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  var Item = sequelize.define("Item", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nome: {
+      type: DataTypes.STRING,
+    },
+    descricao: {
+      type: DataTypes.STRING
+    }
+  }, {
+      tableName: "Item"
+    }
+  )
+
+  Item.associate = function (models) {
+    // Item.belongsTo(models.Usuario, { foreignKey: "usuarioId" })
+  }
+
+  return Item
+}
