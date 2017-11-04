d8afeb1e668a66f08c42e48c7c9e543e5a709389
guimpo <nakaima.prk@gmail.com>
2017-11-04 08:23:53 -0200

    Adicionada tela para upload de arquivos

--- servidor.js
+++ servidor.js
@@ -28,6 +28,18 @@ app.get('/CadastrarProfessor', function (req, res, next) {
   }
 })
 
+app.get('/CadastrarDocumento', function (req, res, next) {
+  try {
+    fs.readFile(__dirname + '/views/cadastrarDocumento.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    })
+  } catch (e) {
+    next(e)
+  }
+})
+
 app.listen(process.env.PORT || 3001, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3001))
 })
